8450b17feb967784d4e8b8ac75d5024c515bb0f6
Arin Paliwal <paliwalarin09@gmail.com>
2023-05-11 15:54:55 +0530

    completed structure

--- frontend/src/scenes/dashboard/index.tsx
+++ frontend/src/scenes/dashboard/index.tsx
@@ -1,8 +1,9 @@
-import DashboardBox from "@/components/DashboardBox";
-import { Box, useMediaQuery, useTheme } from "@mui/material";
+import { Box, useMediaQuery } from "@mui/material";
 import React from "react";
+import Row1 from "./Row1";
+import Row2 from "./Row2";
+import Row3 from "./Row3";
 
-type Props = {};
 const gridTemplateLargeScreens = `
 "a b c"
 "a b c"
@@ -46,9 +47,8 @@ const gridTemplateSmallScreens = `
 "j"
 "j"
 `;
-const Dashboard = (props: Props) => {
+const Dashboard = () => {
   const isAboveMediumScreens = useMediaQuery("(min-width:1200px)");
-  const { palette } = useTheme();
   return (
     <Box
       width="100%"
@@ -69,16 +69,9 @@ const Dashboard = (props: Props) => {
             }
       }
     >
-      <DashboardBox gridArea="a"></DashboardBox>
-      <DashboardBox gridArea="b"></DashboardBox>
-      <DashboardBox gridArea="c"></DashboardBox>
-      <DashboardBox gridArea="d"></DashboardBox>
-      <DashboardBox gridArea="e"></DashboardBox>
-      <DashboardBox gridArea="f"></DashboardBox>
-      <DashboardBox gridArea="g"></DashboardBox>
-      <DashboardBox gridArea="h"></DashboardBox>
-      <DashboardBox gridArea="i"></DashboardBox>
-      <DashboardBox gridArea="j"></DashboardBox>
+      <Row1 />
+      <Row2 />
+      <Row3 />
     </Box>
   );
 };
