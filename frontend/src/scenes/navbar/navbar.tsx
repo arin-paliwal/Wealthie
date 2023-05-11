371bf8323dbf11c355fd855fc7531d83d93f8a58
Arin Paliwal <paliwalarin09@gmail.com>
2023-05-11 15:02:53 +0530

    Added Media queries

--- frontend/src/scenes/navbar/navbar.tsx
+++ frontend/src/scenes/navbar/navbar.tsx
@@ -17,7 +17,7 @@ const Navbar = (props: Props) => {
       <FlexBetween gap="0.75rem">
         <CurrencyRupeeIcon sx={{ fontSize: "20px" }} />
         <Typography variant="h4" fontSize="16px">
-          welthie
+          wealthie
         </Typography>
       </FlexBetween>
 
