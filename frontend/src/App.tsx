1c2f07fb67ca538b546230edac24d8beea8cbf27
Arin Paliwal <paliwalarin09@gmail.com>
2023-05-13 23:04:54 +0530

    .

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,7 +13,7 @@ function App() {
       <BrowserRouter>
         <ThemeProvider theme={theme}>
           <CssBaseline />
-          <Box width="100%" height="10%" padding="1rem 2rem 4rem 2rem">
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
             <Navbar/>
             <Routes>
               <Route path="/" element={<Dashboard/>} />
